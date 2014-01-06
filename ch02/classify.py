109fa36a5d438647ddc19375f00e1c51cbec98c0
Tom Zaragoza <tomdzaragoza@gmail.com>
2014-01-05 22:24:07 -0500

    plotting the data points

---
+++ ch02/classify.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot as plt
+from sklearn.datasets import load_iris
+import numpy as np
+
+# load the data
+data = load_iris()
+features = data['data']
+features_names = data['feature_names']
+target = data['target']
+
+for t, marker, c in zip(xrange(3), ">ox", "rgb"):
+	# plot each class on its own to get different colours markers
+	plt.scatter(features[target== t,0],
+				features[target== t,1],
+				marker=marker,
+				c=c)
