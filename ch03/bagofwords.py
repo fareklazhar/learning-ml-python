ac741f59f9bca4013a704ade391aefd2b3d2df87
Tom Zaragoza <tomdzaragoza@gmail.com>
2014-01-06 21:23:39 -0500

    add in iterating through the different posts to find most similar

--- ch03/bagofwords.py
+++ ch03/bagofwords.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import scipy as sp
 from sklearn.feature_extraction.text import CountVectorizer
 vectorizer = CountVectorizer(min_df=1) # minimum document frequency
 
@@ -17,3 +19,32 @@ X_train = vectorizer.fit_transform(posts)
 num_samples, num_features = X_train.shape
 
 print("#samples: %d, #features: %d" % (num_samples, num_features)) #samples: 5, #features: 25
+print vectorizer.get_feature_names()
+print "With {0} different words".format(len(vectorizer.get_feature_names()))
+
+# goal here is to find the most similar post for the post new_post defined below
+new_post = "imaging database"
+new_post_vec = vectorizer.transform([new_post])
+new_post_array = new_post_vec.toarray() # need it to be the full array if we want to do similarity calculations
+
+# calculate Euclidean distance, of new post with the old posts
+def dist_raw(v1, v2):
+	delta = v1 - v2
+	return sp.linalg.norm(delta.toarray())
+
+best_doc = None
+best_dist = sys.maxint
+best_i = None
+
+for i in range(0, num_samples):
+	post = posts[i] # iterate through the posts
+	if post == new_post:
+		continue
+	post_vec = X_train.getrow(i)
+	d = dist_raw(post_vec, new_post_vec)
+	print "=== Post {0} with dist={1}: {2}".format(i, d, post)
+	if d < best_dist:
+		best_doc = post
+		best_dist = d
+		best_i = i
+print "The best post is '{0}' with dist={1}".format(best_doc, best_dist)
